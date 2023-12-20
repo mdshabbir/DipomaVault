c172a815c7ae30e9963c3ee34bf64afcb1bf5422
Mohammad Shabbir <mdshabbirak47@gmail.com>
2023-12-21 00:17:27 +0530

    new changes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { TeamComponent } from './team/team.component';
@@ -15,6 +15,7 @@ const routes: Routes = [
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'feedback', component: FeedbackComponent },
+  {path:'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PopularSubjectsComponent } from './popular-subjects/popular-subjects.component';
-import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { TeamComponent } from './team/team.component';
 import { ServicesComponent } from './services/services.component';
@@ -15,13 +14,13 @@ import { FeedbackComponent } from './feedback/feedback.component';
 import { SlidingInformationComponent } from './sliding-information/sliding-information.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PopularSubjectsComponent,
-    AboutComponent,
     PortfolioComponent,
     TeamComponent,
     ServicesComponent,
@@ -30,7 +29,8 @@ import { HomeComponent } from './home/home.component';
     FeedbackComponent,
     SlidingInformationComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
